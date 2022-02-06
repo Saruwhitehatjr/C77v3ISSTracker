6eb1c77be9a040ffc5241056e143f1ded4e2a0a5
Saruwhitehatjr <sarurobin@gmail.com>
2022-02-06 19:03:50 +0530

    C77

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MeteorScreen from './screens/Meteor';
+import ISSLocationScreen from './screens/ISSLocation'
+import HomeScreen  from './screens/Home';
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+import 'react-native-gesture-handler'
+ 
+const Stack= createStackNavigator()
 
-export default function App() {
+export default class App extends React.Component {
+  render(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NavigationContainer>
+     <Stack.Navigator initialRouteName='Home' screenOptions={{headerShown:false}}>
+       <Stack.Screen name="Home" component={HomeScreen}/>
+       <Stack.Screen name="ISSLocation" component={ISSLocationScreen}/>
+       <Stack.Screen name="Meteor" component={MeteorScreen}/>
+       
+
+       </Stack.Navigator>
+   
+     </NavigationContainer>
   );
+}
 }
 
 const styles = StyleSheet.create({
